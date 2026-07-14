523e90d5bb0218d4cd66aa6fc6618b68356d5281
Jagadeep-43 <2k25ecx15@kiot.ac.in>
2026-07-14 14:47:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,752 @@
 const buses = [
-    {
-        route: "01",
-        stop: "KR Thoppur",
-        arrival: "8:40 AM"
-    },
-    {
-        route: "02",
-        stop: "Bhavani Bus Stand",
-        arrival: "8:40 AM"
-    }
+    const buses = [
+
+{
+    number: "01",
+    route: "K R Thoppur to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "KR Thoppur",
+        "Steel Plant",
+        "Maramangalathu Patty",
+        "Medical College",
+        "Pall Pannai",
+        "Sithanoor",
+        "Solampalam",
+        "KIOT"
+    ]
+},
+
+{
+    number: "02",
+    route: "Bhavani Bus Stand to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Bhavani Bus Stand",
+        "Anthiyur Pirivu",
+        "Bhavani Old Bus Stand",
+        "Kalingarayanpalayam",
+        "Lakshmi Nagar",
+        "Colony Hospital",
+        "Komarapalayam",
+        "Aangur Road",
+        "Rajam Theater",
+        "Kathari Pirivu",
+        "Valaiyakaranur",
+        "Pallakkapalayam",
+        "Pachampalayam",
+        "Kavandanur",
+        "ICL",
+        "Vaikundham",
+        "Thalaiyur",
+        "KIOT"
+    ]
+},
+
+{
+    number: "03",
+    route: "Salem Prabath to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Prabath",
+        "KIOT"
+    ]
+},
+
+{
+    number: "04",
+    route: "Thevur to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Kanniyalam Patti",
+        "Thevur",
+        "Maylampatti",
+        "Palakaran Kottai",
+        "Kullampatti",
+        "Moolapathai",
+        "Vellari Velli",
+        "Housing Board",
+        "Edappadi",
+        "Vellandi Valasu",
+        "Sunnambu Soolai",
+        "Rangampalayam",
+        "Perivu Road",
+        "Vellaiyampalayam",
+        "Kuthadi Palayam",
+        "Kalipatti Perivu Road",
+        "R.K. Theater & Mill",
+        "KIOT"
+    ]
+},
+
+{
+    number: "05",
+    route: "Aalampalayam to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Aalampalayam",
+        "SPB Colony",
+        "Anna Nagar",
+        "K.S.R College",
+        "Varappalayam",
+        "Thokkavadi",
+        "Kootapalli",
+        "Rajagoundampalayam",
+        "Santha Pettai",
+        "T.Code New Old Bus Stand",
+        "Setharampalayam",
+        "Sangagiri RS",
+        "Kolikaal Natham",
+        "Vadugapatti",
+        "Kancham Puthur",
+        "Thalaiyur",
+        "R.K. Theater & Mill",
+        "KIOT"
+    ]
+},
+
+{
+    number: "06",
+    route: "Namakkal to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Namakkal Bus Stand",
+        "Salem Road",
+        "Chellappampatti",
+        "Buthansanthai",
+        "Kalangani",
+        "Puthu Chathiram",
+        "Andaloor Gate",
+        "Gurusamipalayam",
+        "Vaiyappam Malai",
+        "Aathumedu",
+        "Kottapalayam",
+        "M. Mettupalayam",
+        "Mallai Santhapavadi",
+        "Mallasamuthiram",
+        "Kalippati",
+        "Attayampatty",
+        "KIOT"
+    ]
+},
+
+{
+    number: "07",
+    route: "Sivadhapuram to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Sivadhapuram",
+        "Panakadu",
+        "Thirumalaigiri",
+        "Nallampatty",
+        "Murungapatty",
+        "Chittar Kovil",
+        "K.K Nagar",
+        "Kadaiyam Patty",
+        "Elampillai Santhi Studio",
+        "Elampillai Bus Stand",
+        "Perumagovundampatti",
+        "Vembadithalam",
+        "KIOT"
+    ]
+},
+
+{
+    number: "08",
+    route: "Pakkanadu to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Aadaiyur",
+        "Pakkanadu",
+        "Vannakuttai",
+        "Sowriyur",
+        "Alamaram",
+        "Jalagandapuram",
+        "Kapprathampatti",
+        "Panikkanur",
+        "Etikuttai Medu",
+        "Aegapuram",
+        "Koothampalayam",
+        "Thailampatti",
+        "Thadikaranur",
+        "Theppakuttai",
+        "Perumagovundampatti",
+        "Vembadithalam",
+        "KIOT"
+    ]
+},
+
+{
+    number: "09",
+    route: "Panikkanur to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Panikkanur",
+        "Samuthiram",
+        "Pudhupalayam",
+        "Sadaiyampalayam",
+        "Koranampatty",
+        "Moolappathai",
+        "Pallipatti",
+        "Katchipalli",
+        "Konganapuram",
+        "Erumapatti Pirivu",
+        "Mattampatty",
+        "Kannantheri",
+        "Ondipannai",
+        "Sunda Mettur",
+        "Alaganur",
+        "Magudanchavadi",
+        "KIOT"
+    ]
+},
+
+{
+    number: "10",
+    route: "Salem Yercaud Adivaaram to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Salem Yercaud Adivaaram",
+        "Law College",
+        "Kollapatti",
+        "Gorimadu",
+        "Housing Board",
+        "Central Jail",
+        "Hasthampatty",
+        "Vincent",
+        "KIOT"
+    ]
+}
+
+];
+,
+
+{
+    number: "11",
+    route: "Kannakurichi to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Kannakurichi",
+        "Housing Unit",
+        "Iyenthar Maaligahi",
+        "Ramanathapuram",
+        "Chinna Muniyappan Kovil",
+        "Chinnathirupathi",
+        "Manakkadu",
+        "KIOT"
+    ]
+},
+
+{
+    number: "12",
+    route: "Rasipuram to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Rasipuram Old Bus Stand",
+        "SRV School",
+        "Attayampatty Pirivu",
+        "Athanoor",
+        "Aaramathampalayam",
+        "Oolaipatti",
+        "Nadupatti",
+        "Vellapillayar Kovil",
+        "Vennandur",
+        "Anna Silai",
+        "Kamarajar Silai",
+        "Sappaiyapuram",
+        "Ramalingam Hospital",
+        "Mariyamman Kovil",
+        "KIOT"
+    ]
+},
+
+{
+    number: "13",
+    route: "Salem Vijaya Hospital to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Vijaya Hospital",
+        "Ramakrishna Park",
+        "Ramakrishna Road",
+        "Saradha College",
+        "Fair Lands",
+        "Alagapuram",
+        "KIOT"
+    ]
+},
+
+{
+    number: "14",
+    route: "Salem Pilliyar Nagar to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Salem Pilliyar Nagar",
+        "T.M.S. Set",
+        "Pattakovil",
+        "Rajaganapathi Kovil",
+        "CSI School & Municipality Office",
+        "KIOT"
+    ]
+},
+
+{
+    number: "15",
+    route: "Salem Ammapettai to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Jothi School",
+        "Holy Cross School",
+        "Udayapatty By-pass",
+        "Kumaragiri",
+        "KIOT"
+    ]
+},
+
+{
+    number: "16",
+    route: "Aathur to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Aathur",
+        "Narasingapuram",
+        "Selliyampalayam",
+        "Kothambadi",
+        "Pethanayakkanpalayam",
+        "Putharagovundampalayam",
+        "Valappadi",
+        "Muthampatti",
+        "Sashanchavadi",
+        "Mettupatti",
+        "KIOT"
+    ]
+},
+
+{
+    number: "17",
+    route: "Sowdeshwari College to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Kondalampatty Bye Pass",
+        "Kondalampatty",
+        "Sandhai Pettai",
+        "Neikarapatti",
+        "Poolavari Pirivu",
+        "Ariyanoor",
+        "Veerapandi",
+        "Palampatty",
+        "Birogy",
+        "S-Palam",
+        "Nainampatti",
+        "KIOT"
+    ]
+},
+
+{
+    number: "18",
+    route: "Salem Kuppanur to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Salem Kuppanur",
+        "Valasaiur",
+        "Pallipatti",
+        "Veemanuer",
+        "Veeranam",
+        "Manarpalayam Pirivu",
+        "Vaikalpattari",
+        "KIOT"
+    ]
+},
+
+{
+    number: "19",
+    route: "Vanavasi to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Vanavasi",
+        "Soorapalli",
+        "Jalakandapuram",
+        "Selavadi",
+        "Tharamangalam",
+        "Thuttam Patti",
+        "Chinna Pillaiyer Pananchary",
+        "Madathur",
+        "Edangana Salai",
+        "Karikadai",
+        "KIOT"
+    ]
+},
+
+{
+    number: "20",
+    route: "Chekkapatti to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Chekkapatti",
+        "Elachipalayam",
+        "Kumaramangalam",
+        "Valarai Gate",
+        "T.Gode Bus Stand",
+        "Kailasam Palayam",
+        "Chinnathambi Palayam",
+        "Vattur Road",
+        "More Palayam",
+        "Sembampalayam",
+        "Kallukadai",
+        "TCMS",
+        "Suriyagoundampalayam",
+        "Malasamutharam High School",
+        "Marulaiyampalayam",
+        "KIOT"
+    ]
+}
+,
+
+{
+    number: "21",
+    route: "Mallur to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Mallur",
+        "Earikarai",
+        "Santhiur",
+        "Nilavara Patti",
+        "Alagu Nagar",
+        "KIOT"
+    ]
+},
+
+{
+    number: "22",
+    route: "Mettur to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Mettur",
+        "Madha Kovil",
+        "Chinna Park",
+        "Sam Palli",
+        "Raman Nagar",
+        "Kunjandiyer",
+        "Verakkal",
+        "Nanga Valli",
+        "Periya Sorakai",
+        "Kullanur",
+        "Seranganur",
+        "Chinnapanpatti",
+        "Elampillai",
+        "KIOT"
+    ]
+},
+
+{
+    number: "23",
+    route: "Perumal Kovil Medu to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Perumal Kovil Medu",
+        "Mutthanayacken Patty",
+        "Paagal Patty",
+        "Reddy Patty",
+        "Junction",
+        "T.P.T (Sona College)",
+        "State Bank Colony",
+        "KIOT"
+    ]
+},
+
+{
+    number: "24",
+    route: "Pillukadai to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Pillukadai",
+        "Dhadagapatty Gate",
+        "KIOT"
+    ]
+},
+
+{
+    number: "25",
+    route: "Mangalapuram to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Mangalapuram",
+        "Mettala",
+        "Namagiripattai",
+        "Kakaveri",
+        "Rasipuram TVS",
+        "Rasipuram Old Bus Stand",
+        "SRV School",
+        "Masakalipatti Pirivu",
+        "KIOT"
+    ]
+},
+
+{
+    number: "26",
+    route: "Ammapattai to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Ammapattai",
+        "Ammapattai Jothi",
+        "Saradhas",
+        "Easan",
+        "KIOT"
+    ]
+},
+
+{
+    number: "27",
+    route: "MAM Petrol Bunk to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "MAM Petrol Bunk",
+        "Meachari",
+        "Sathapadi",
+        "Kamaneri",
+        "Sinthamaniyur",
+        "Palikadai",
+        "5-Myl",
+        "Thulasampatti",
+        "Amarakunthi",
+        "Tharamangalam",
+        "Post Office",
+        "KIOT"
+    ]
+},
+
+{
+    number: "28",
+    route: "Salem Prabath to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Prabath",
+        "KIOT"
+    ]
+},
+
+{
+    number: "29",
+    route: "Vellalapatti to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Vellalapatti",
+        "Karuppur",
+        "Karumbaalai",
+        "Dalmiya",
+        "Mamangam",
+        "Sankar Theatre",
+        "Kuranguchavati",
+        "Seeragapady",
+        "KIOT"
+    ]
+},
+
+{
+    number: "30",
+    route: "Erode Bus Stand to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Erode Bus Stand",
+        "Karungalpalayam",
+        "Register Office",
+        "Pallipalayam",
+        "Otta Methai",
+        "Veppadai",
+        "Sanniyasi Patti Railway Gate",
+        "Bhavani Pirivu Road",
+        "Pacha Kadu",
+        "Sangari Police Station",
+        "Sangari Old Bus Stand",
+        "Sangari New Bus Stand",
+        "Konganapuram Pirivu",
+        "KIOT"
+    ]
+}
+,
+
+{
+    number: "31",
+    route: "Ammapettai to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Ammapettai (Jothi Stop)",
+        "Guruvayur Kovil",
+        "Palaniyandi Hospital",
+        "KIOT"
+    ]
+},
+
+{
+    number: "32",
+    route: "Salem Four Roads to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Salem Four Roads",
+        "Salem New Bus Stand",
+        "Annapoorna Hospital",
+        "KIOT"
+    ]
+},
+
+{
+    number: "33",
+    route: "Kootathupatti to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Kootathupatti",
+        "A N Mangalam",
+        "Kullampatti",
+        "A Pattinam HR School",
+        "Ayotheyapattinam",
+        "Udayapatti",
+        "Seelanaickenpatty Bye Pass",
+        "KIOT"
+    ]
+},
+
+{
+    number: "34",
+    route: "Salem Old Bus Stand to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Salem Old Bus Stand",
+        "Javulikkadai",
+        "Annathanapatti",
+        "Kalyana Mandabam",
+        "Power House",
+        "KIOT"
+    ]
+},
+
+{
+    number: "35",
+    route: "Ponnamapettai Gate to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Chennai Cakes",
+        "Puthu Mariyamman Kovil",
+        "Water Tank",
+        "Ponnamapettai Gate",
+        "Guruvayurappan Kovil",
+        "KIOT"
+    ]
+},
+
+{
+    number: "36",
+    route: "Pillukadai to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Pillukadai",
+        "Dhadagapatty Gate",
+        "KIOT"
+    ]
+},
+
+{
+    number: "37",
+    route: "Pudhu Road to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Pudhu Road",
+        "Reddipatti By Pass",
+        "BPC Gas Bunk",
+        "AVR Roundana",
+        "RTO By Pass",
+        "Neikarapatti",
+        "Seerapadi",
+        "Rakipatti",
+        "KIOT"
+    ]
+},
+
+{
+    number: "38",
+    route: "Vincent to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Vincent",
+        "Gandhi Road",
+        "Sundar Lodge - 7 Arts",
+        "Apsara Down",
+        "Nethimedu",
+        "KIOT"
+    ]
+},
+
+{
+    number: "39",
+    route: "Dharmapuri to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Dharmapuri",
+        "Nallampalli",
+        "Thomparakkampatti",
+        "Palayamputhur Toll Gate",
+        "Thoppur",
+        "Jodu Kuli",
+        "Thevatipatti",
+        "Poosari Patti",
+        "Pannapatti",
+        "Kamalapuram",
+        "Omalur Bus Stand",
+        "Puliyampatty",
+        "Toll Gate",
+        "Periyar University",
+        "Karuppur Engineering College",
+        "KIOT"
+    ]
+},
+
+{
+    number: "40",
+    route: "Periyar University to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Toll Gate (Periyar University)",
+        "Govt Engineering College",
+        "Karumbalai",
+        "Dalmia Board",
+        "Five Roads",
+        "5 Theatre (ARRS)",
+        "3 Road",
+        "Jawahar Mill",
+        "Thiruvagoundanur Bye Pass",
+        "KIOT"
+    ]
+},
+
+{
+    number: "41",
+    route: "Panamarathupatty to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Panamarathupatty",
+        "Panamarathupatty Pirivu",
+        "Seelanaickenpatty Bye Pass",
+        "NPR School",
+        "Divya Theatre",
+        "Kondalampatty Bye Pass",
+        "KIOT"
+    ]
+},
+
+{
+    number: "42",
+    route: "Mettupatty Toll Gate to KIOT",
+    arrival: "8:40 AM",
+    stops: [
+        "Mettupatty Toll Gate",
+        "Karipatty",
+        "Minnampalli",
+        "Masinaickenpatty",
+        "Erumapalayam",
+        "Sanniyasigundu",
+        "KIOT"
+    ]
+}
+
+];
 ];
 
 function displayBuses(list) {
