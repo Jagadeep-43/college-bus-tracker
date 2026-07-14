ec998b201781140c9521f93d90fab9b8cf31a4eb
Jagadeep-43 <2k25ecx15@kiot.ac.in>
2026-07-14 14:38:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-document.getElementById("busList").innerHTML = `
-    <div class="bus-card">
-        <h3>Route 01</h3>
-        <p>KR Thoppur → KIOT</p>
-        <p>Arrival: 8:40 AM</p>
-    </div>
+const buses = [
+    {
+        route: "01",
+        stop: "KR Thoppur",
+        arrival: "8:40 AM"
+    },
+    {
+        route: "02",
+        stop: "Bhavani Bus Stand",
+        arrival: "8:40 AM"
+    }
+];
 
-    <div class="bus-card">
-        <h3>Route 02</h3>
-        <p>Bhavani Bus Stand → KIOT</p>
-        <p>Arrival: 8:40 AM</p>
-    </div>
-`;
+function displayBuses(list) {
+    const busList = document.getElementById("busList");
+    busList.innerHTML = "";
+
+    list.forEach(bus => {
+        busList.innerHTML += `
+            <div class="bus-card">
+                <h3>Route ${bus.route}</h3>
+                <p><strong>Stop:</strong> ${bus.stop}</p>
+                <p><strong>Arrival:</strong> ${bus.arrival}</p>
+            </div>
+        `;
+    });
+}
+
+function searchBus() {
+    const text = document
+        .getElementById("searchBox")
+        .value
+        .toLowerCase();
+
+    const filtered = buses.filter(bus =>
+        bus.route.toLowerCase().includes(text) ||
+        bus.stop.toLowerCase().includes(text)
+    );
+
+    displayBuses(filtered);
+}
+
+displayBuses(buses);
