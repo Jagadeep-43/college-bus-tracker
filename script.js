7b70f5f4dc96196e6a736f03647e0077b53d39d8
Jagadeep-43 <2k25ecx15@kiot.ac.in>
2026-07-14 14:33:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("College Bus Tracker Loaded");
+document.getElementById("busList").innerHTML = `
+    <div class="bus-card">
+        <h3>Route 01</h3>
+        <p>KR Thoppur → KIOT</p>
+        <p>Arrival: 8:40 AM</p>
+    </div>
+
+    <div class="bus-card">
+        <h3>Route 02</h3>
+        <p>Bhavani Bus Stand → KIOT</p>
+        <p>Arrival: 8:40 AM</p>
+    </div>
+`;
